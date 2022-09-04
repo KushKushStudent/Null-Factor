1ba80b53e00ce7e5717e2bb6f93fb4860d94bc2e
KushKushStudent <80677966+KushKushStudent@users.noreply.github.com>
2022-09-04 09:37:29 +0200

    guns

--- Assets/Scripts/Player/GunSwapController.cs
+++ Assets/Scripts/Player/GunSwapController.cs
@@ -6,14 +6,19 @@ public class GunSwapController : MonoBehaviour
 {
     public int gunNumber;
     public GameObject[] guns;
+    public GameObject[] gunVisuals;
     // Start is called before the first frame update
     void Start()
     {
         gunNumber = 0;
         guns[0].SetActive(true); 
+        gunVisuals[0].SetActive(true); 
         guns[1].SetActive(false); 
         guns[2].SetActive(false); 
         guns[3].SetActive(false);
+        gunVisuals[1].SetActive(false); 
+        gunVisuals [2].SetActive(false); 
+        gunVisuals[3].SetActive(false);
     }
 
     // Update is called once per frame
@@ -21,39 +26,40 @@ public class GunSwapController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Alpha1)) //gun 1
         {
-            for (int i=0;i<guns.Length;i++) 
-            { 
-                guns[i].SetActive(false);
-                
-            }
+            DeactivateGunVisuals();
             guns[0].SetActive(true);
+            gunVisuals[0].SetActive(true);
         } 
         if (Input.GetKeyDown(KeyCode.Alpha2)) //gun 2
         {
-            for (int i=0;i<guns.Length;i++) 
-            { 
-                guns[i].SetActive(false);
-            }
+            DeactivateGunVisuals();
             guns[1].SetActive(true);
+            gunVisuals[1].SetActive(true);
         } 
         
         if (Input.GetKeyDown(KeyCode.Alpha3)) //gun 3
         {
-            for (int i=0;i<guns.Length;i++) 
-            { 
-                guns[i].SetActive(false);
-            }
+            DeactivateGunVisuals();
             guns[2].SetActive(true);
+            gunVisuals[2].SetActive(true);
         }
         
         
         if (Input.GetKeyDown(KeyCode.Alpha4)) //gun 4
         {
-            for (int i=0;i<guns.Length;i++) 
-            { 
-                guns[i].SetActive(false);
-            }
+            DeactivateGunVisuals();
             guns[3].SetActive(true);
+            gunVisuals[3].SetActive(true);
         }
+    }
+    public void DeactivateGunVisuals() 
+    {
+        for (int i = 0; i < guns.Length; i++)
+        {
+            guns[i].SetActive(false);
+            gunVisuals[i].SetActive(false);
+
+        }
+
     }
 }
